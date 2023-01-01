9e8145730e431999adc899ebbf68af74ad2b4a06
surazkarn <surazkarn8848@gmail.com>
2023-01-01 20:28:15 +0530

    reminder express server setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+    const app= express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`server started at port ${PORT}`);
+    })
+    
+}
+
+setupAndStartServer();
